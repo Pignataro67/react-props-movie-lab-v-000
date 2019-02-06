92c9c3f26a62652e63f7c1dadacdbbdcb37b7de6
Pat  Pignataro <pat6284@msn.com>
2019-02-06 19:32:06 +0000

    Automatically backed up by Learn

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -12,7 +12,7 @@ export default class CardBack extends Component {
 
   generateRatingElement = () => {
     if (this.props.IMDBRating === null) {
-      return <h4>'No Rating Found'</h4>
+      return <h4>No Rating Found</h4>
     } else {
       return <img src={imgMapper[this.props.IMDBRrating]} alt="" />
     }
